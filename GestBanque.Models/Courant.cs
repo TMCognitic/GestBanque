28dc92a24cc0d6db6b8cc1d31423a493b83c9f43
Thierry Morre <thierry.morre@cognitic.be>
2025-10-22 12:25:18 +0200

    Ajoutez des fichiers projet.

---
+++ GestBanque.Models/Courant.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestBanque.Models
+{
+    public class Courant
+    {
+        private double _ligneDeCredit;
+        public string Numero { get; set; }
+        public double Solde { get; private set; }
+        public double LigneDeCredit
+        {
+            get
+            {
+                return _ligneDeCredit;
+            }
+
+            set
+            {
+                if (value < 0)
+                    return;
+
+                _ligneDeCredit = value;
+            }
+        }
+        public Personne Titulaire { get; set; }
+
+        public void Depot(double montant)
+        {
+            if (montant <= 0)
+            {
+                Console.WriteLine("Montant invalide");
+                return;
+            }
+
+            Solde += montant;
+        }
+
+        public void Retrait(double montant)
+        {
+            if (montant <= 0) 
+            {
+                Console.WriteLine("Montant invalide");
+                return; 
+            }
+            if(Solde - montant < -LigneDeCredit) 
+            {
+                Console.WriteLine("Solde insuffisant");
+                return; 
+            }
+
+            Solde -= montant;
+        }
+    }
+}
---
+++ GestBanque/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Personne johnDoe = new Personne() 
+{ 
+    Nom = "Doe", 
+    Prenom = "John", 
+    DateNaiss = new DateTime(1970, 1, 1) 
+};
+
+Courant courant = new Courant()
+{
+    Numero = "0001",
+    LigneDeCredit = 300,
+    Titulaire = johnDoe
+};
+
+Console.WriteLine($"Depot de -100");
+courant.Depot(-100);
+Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Depot de 100");
+courant.Depot(100);
+Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Retrait de -100");
+courant.Retrait(-100);
+Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Retrait de 300");
+courant.Retrait(300);
+Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Retrait de 500");
+courant.Retrait(500);
+Console.WriteLine($"Nouveau solde : {courant.Solde}");
